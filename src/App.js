a94f113debc7e23d10cb11dc18acd1fb6c24fe8b
Avey <romeo.pruvost3007@gmail.com>
2021-05-04 14:31:58 +0200

    Adding ul of Comments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import Person from './Person'
 
 
 class App extends Component {
+state = {
+  comments: [
+    { name: "JB", message: "Youhou la famille!"},
+    { name: "Kirikou", message: "Je ne suis pas grand mais je suis vaillant"}
+    ]
+}
+
   render() {
+    const commentsJSX = this.state.comments.map(person => {
+      return <p>{person.name} a dit: {person.message}</p>
+    })
     return (
       <div className="App">
         <h1>Coucou les gens</h1>
@@ -13,9 +23,11 @@ class App extends Component {
         <Counter verb = "Courru"  rest="Km(s)"/>
         <Counter verb = "Acheté" rest="produits" />
         <Counter verb = "Recommencé" rest="fois"/>
-
         <Person firstname="Jean" lastname="Michel"/>
         <Person firstname="Michel" lastname="Jean"/>
+        <ul>
+          {commentsJSX}
+        </ul>
       </div>
     );
   }
